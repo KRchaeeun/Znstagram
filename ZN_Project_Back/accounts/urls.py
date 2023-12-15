c99def5b19db0a3e846cde1e302197c669c3b011
ljh2375 <ljh23705@gmail.com>
2023-12-15 20:12:32 +0900

    [CREATE] account model

--- ZN_Project_Back/accounts/urls.py
+++ ZN_Project_Back/accounts/urls.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+urlpatterns = [
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+]
