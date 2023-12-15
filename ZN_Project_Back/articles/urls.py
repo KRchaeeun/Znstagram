e5f2aa5221685b5fe77b2f92d86781cdf9332d9f
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:49:06 +0900

    [ADD] annotated added

--- ZN_Project_Back/articles/urls.py
+++ ZN_Project_Back/articles/urls.py
@@ -6,7 +6,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.articles_list, name='article_list'),  # 전체 게시글 목록
-    path('<int:article_pk>/', views.article_detail, name='article_detail'),
+    path('<int:article_pk>/', views.article_detail, name='article_detail'),  # 특정 게시글 상세 보기
     path('<int:article_pk>/comments/', views.comment_create),
     path('comments/<int:comment_pk>/', views.comment_ud),
 ]
