687fc498f4f08bcc02ff9cf4e1f1f50cd787fd4b
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:00:28 +0900

    [ADD] annotated added: image file optimization

--- ZN_Project_Back/articles/models.py
+++ ZN_Project_Back/articles/models.py
@@ -15,6 +15,13 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     location = models.CharField(max_length=255, blank=True)  # 사용자가 포스팅한 위치를 설정할 수 있는 위치 필드 추가
     image = models.ImageField(blank=True, upload_to = 'images/')
+    # 이미지 파일 크기를 최적화 코드가 필요
+    # 이미지를 자동으로 압축하고 크기를 조정하는 방법은 
+    # 1. Pillow 라이브러리 사용
+    # 2. Custom ImageField
+    # 3. 저장 시점에서 이미지 처리
+    # 4. django-imagekit과 같은 서드 파티 라이브러리 사용
+    
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
