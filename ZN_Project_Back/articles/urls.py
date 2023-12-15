656242a0cbf30c40a054ea37884988c4a08b4498
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:48:57 +0900

    [UPDATE] typo annotated deleted

--- ZN_Project_Back/articles/urls.py
+++ ZN_Project_Back/articles/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    path('', views.articles_list, name='article_list'),  # # 전체 게시글 목록
+    path('', views.articles_list, name='article_list'),  # 전체 게시글 목록
     path('<int:article_pk>/', views.article_detail, name='article_detail'),
     path('<int:article_pk>/comments/', views.comment_create),
     path('comments/<int:comment_pk>/', views.comment_ud),
