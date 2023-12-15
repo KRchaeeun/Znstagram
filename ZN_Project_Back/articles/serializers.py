22d36abc202d6b227611152afd462d2310b618f0
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:44:17 +0900

    [ADD] annotated added

--- ZN_Project_Back/articles/serializers.py
+++ ZN_Project_Back/articles/serializers.py
@@ -9,6 +9,12 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ArticleLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleLike
+        fields = '__all__'
+
+
 # 전체 게시글 시리얼라이져
 class ArticleListSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField()  # 좋아요 수를 나타내는 필드 추가
