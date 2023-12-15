ba04cbd194bc6c72b289ffb489479b35f0d6d1d1
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:19:36 +0900

    [UPDATE] get_articels added

--- ZN_Project_Back/articles/models.py
+++ ZN_Project_Back/articles/models.py
@@ -7,6 +7,10 @@ class Tag(models.Model):
     # content = models.TextField(unique=True)
     content = models.CharField(max_length=100, unique=True)  # 태그는 일반적으로 짧으므로 TextField 대신 CharField 사용
 
+    def get_articles(self):
+        return self.article_set.all()
+
+
 class Article(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
