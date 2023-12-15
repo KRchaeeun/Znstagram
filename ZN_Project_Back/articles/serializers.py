00280d22e88957029250326cc5bc59e09e797bf1
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:31:56 +0900

    [UPDATE] import Tag model

--- ZN_Project_Back/articles/serializers.py
+++ ZN_Project_Back/articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Comment
+from .models import Article, Comment, Tag
 
 class ArticleListSerializer(serializers.ModelSerializer):
 
