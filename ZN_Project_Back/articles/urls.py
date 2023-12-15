9f9af0c17ab374c715047d640a6b68992bfa1099
Chaeeun Lee <cheeun0903@gmail.com>
2023-12-15 19:50:03 +0900

    [UPDATE] views name changed

--- ZN_Project_Back/articles/urls.py
+++ ZN_Project_Back/articles/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.articles_list, name='article_list'),  # 전체 게시글 목록
     path('<int:article_pk>/', views.article_detail, name='article_detail'),  # 특정 게시글 상세 보기
     path('<int:article_pk>/comments/', views.comment_create, name='comment_create'),  # 게시글에 댓글 추가
-    path('comments/<int:comment_pk>/', views.comment_ud),
+    path('comments/<int:comment_pk>/', views.comment_update),
 ]
